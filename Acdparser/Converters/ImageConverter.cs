88f0f36e6e90e88dce038cc54b2a302a356c92b3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-12 19:33:45 +0100

    Update ImageConverter.cs

--- Acdparser/Converters/ImageConverter.cs
+++ Acdparser/Converters/ImageConverter.cs
@@ -26,66 +26,71 @@ public class ImageConverter : IValueConverter
     public static void ReadBmp(string basePath, string fileName)
     {
         var imagePath = GetImagePath(basePath, fileName);
-        if (File.Exists(imagePath))
+        if (!File.Exists(imagePath))
         {
-            // Read the BMP file
-            using var fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
-            using var br = new BinaryReader(fs);
+            return;
+        }
 
-            // Read the header
-            var bfType = br.ReadUInt16();
-            var bfSize = br.ReadInt32();
-            var bfReserved1 = br.ReadUInt16();
-            var bfReserved2 = br.ReadUInt16();
-            var bfOffBits = br.ReadInt32();
+        // Read the BMP file
+        using var fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+        using var br = new BinaryReader(fs);
 
-            // Read the info header
-            var biSize = br.ReadInt32();
-            var biWidth = br.ReadInt32();
-            var biHeight = br.ReadInt32();
-            var biPlanes = br.ReadUInt16();
-            var biBitCount = br.ReadUInt16();
-            var biCompression = br.ReadInt32();
-            var biSizeImage = br.ReadInt32();
-            var biXPelsPerMeter = br.ReadInt32();
-            var biYPelsPerMeter = br.ReadInt32();
-            var biClrUsed = br.ReadInt32();
-            var biClrImportant = br.ReadInt32();
+        // Read the header
+        var bfType = br.ReadUInt16();
+        var bfSize = br.ReadInt32();
+        var bfReserved1 = br.ReadUInt16();
+        var bfReserved2 = br.ReadUInt16();
+        var bfOffBits = br.ReadInt32();
 
-            // Read the data
-            var bmpData = br.ReadBytes((int)br.BaseStream.Length);
+        // Read the info header
+        var biSize = br.ReadInt32();
+        var biWidth = br.ReadInt32();
+        var biHeight = br.ReadInt32();
+        var biPlanes = br.ReadUInt16();
+        var biBitCount = br.ReadUInt16();
+        var biCompression = br.ReadInt32();
+        var biSizeImage = br.ReadInt32();
+        var biXPelsPerMeter = br.ReadInt32();
+        var biYPelsPerMeter = br.ReadInt32();
+        var biClrUsed = br.ReadInt32();
+        var biClrImportant = br.ReadInt32();
 
-            if (biBitCount == 8)
+        // Read the data
+        var bmpData = br.ReadBytes((int)br.BaseStream.Length);
+
+        if (biBitCount == 8)
+        {
+            biClrUsed = 256;
+        }
+
+        // Read the color table
+        var colorTableSize = biClrUsed * 4;
+        var rgb = new RGB[biClrUsed];
+        if (colorTableSize > 0)
+        {
+            byte[] colorTable = new byte[colorTableSize];
+            Array.Copy(bmpData, 0, colorTable, 0, colorTableSize);
+
+            for (var i = 0; i < biClrUsed; i++)
             {
-                biClrUsed = 256;
-            }
-
-            // Read the color table
-            var colorTableSize = biClrUsed * 4;
-            var rgb = new RGB[biClrUsed];
-            if (colorTableSize > 0)
-            {
-                byte[] colorTable = new byte[colorTableSize];
-                Array.Copy(bmpData, 0, colorTable, 0, colorTableSize);
-
-                for (var i = 0; i < biClrUsed; i++)
+                rgb[i] = new RGB
                 {
-                    rgb[i] = new RGB
-                    {
-                        B = colorTable[i * 4],
-                        G = colorTable[i * 4 + 1],
-                        R = colorTable[i * 4 + 2],
-                    };
-                }
+                    B = colorTable[i * 4],
+                    G = colorTable[i * 4 + 1],
+                    R = colorTable[i * 4 + 2],
+                };
             }
+        }
+        
+        // TODO: Use rgb array to get DefineCharacter.Transparency color base on index.
 
-            // Read the pixel data
-            var stride = (biWidth * biBitCount + 7) / 8;
-            var bmpPixels = new byte[stride * biHeight];
-            for (var i = 0; i < biHeight; i++)
-            {
-                Array.Copy(bmpData, colorTableSize + i * stride, bmpPixels, i * stride, stride);
-            }
+        // Read the pixel data
+        // In case of color table those are numbers 0..255 (256 bytes total)
+        var stride = (biWidth * biBitCount + 7) / 8;
+        var bmpPixels = new byte[stride * biHeight];
+        for (var i = 0; i < biHeight; i++)
+        {
+            Array.Copy(bmpData, colorTableSize + i * stride, bmpPixels, i * stride, stride);
         }
     }
     
