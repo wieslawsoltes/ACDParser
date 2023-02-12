6048de6c6edba33ba7115c6436a4a96e29f34d94
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-12 12:05:15 +0100

    Create ImageConverter.cs

---
+++ Acdparser/Converters/ImageConverter.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Avalonia;
+using Avalonia.Data.Converters;
+using SkiaSharp;
+
+namespace Acdparser;
+
+public class ImageConverter : IValueConverter
+{
+    public static ImageConverter Instance = new();
+
+    public static string BasePath = "";
+
+    private static Dictionary<string, SKBitmap?> ImageCache = new();
+
+    public static SKBitmap? ImageLoader(string basePath, string fileName)
+    {
+        var imagePath = OperatingSystem.IsWindows() 
+            ? $"{basePath}\\{fileName}" 
+            : $"{basePath}/{fileName.Replace('\\', '/')}";
+        if (File.Exists(imagePath))
+        {
+            using var stream = File.Open(imagePath, FileMode.Open);
+            var bitmap = SKBitmap.Decode(stream);
+            return bitmap;
+        }
+
+        return null;
+    }
+
+    public static SKBitmap? ToBitmap(string fileName)
+    {
+        if (ImageCache.TryGetValue(fileName, out var bitmap))
+        {
+            return bitmap;
+        }
+        else
+        {
+            bitmap = ImageLoader(BasePath, fileName);
+            if (bitmap is { })
+            {
+                ImageCache[fileName] = bitmap;
+                return bitmap;
+            }
+        }
+
+        return null;
+    }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string fileName)
+        {
+            var bitmap = ToBitmap(fileName);
+            if (bitmap is { })
+            {
+                return bitmap;
+            }
+        }
+
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
