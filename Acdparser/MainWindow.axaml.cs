9624a563fe936b4fb3f949003351a44abf26debd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-12 11:19:50 +0100

    Update MainWindow.axaml.cs

--- Acdparser/MainWindow.axaml.cs
+++ Acdparser/MainWindow.axaml.cs
@@ -1,10 +1,67 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data.Converters;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using SkiaSharp;
 
 namespace Acdparser;
+
+public class ImageConverter : IValueConverter
+{
+    public static ImageConverter Instance = new();
+
+    public static string BasePath = "";
+
+    private static Dictionary<string, SKBitmap?> ImageCache = new();
+
+    private static SKBitmap? ImageLoader(string basePath, string fileName)
+    {
+        var imagePath = OperatingSystem.IsWindows() 
+            ? $"{basePath}\\{fileName}" 
+            : $"{basePath}/{fileName.Replace('\\', '/')}";
+        if (File.Exists(imagePath))
+        {
+            using var stream = File.Open(imagePath, FileMode.Open);
+            var bitmap = SKBitmap.Decode(stream);
+            return bitmap;
+        }
+
+        return null;
+    }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string fileName)
+        {
+            if (ImageCache.TryGetValue(fileName, out var bitmap))
+            {
+                return bitmap;
+            }
+            else
+            {
+                bitmap = ImageLoader(BasePath, fileName);
+                if (bitmap is { })
+                {
+                    ImageCache[fileName] = bitmap;
+                    return bitmap;
+                }
+            }
+        }
+
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 public partial class MainWindow : Window
 {
@@ -13,10 +70,8 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    private void ParseAsdFile()
+    private void ParseAsdFile(string path)
     {
-        //var path = @"c:\Users\Administrator\Downloads\ACS\clippitMS\CLIPPIT ACS Decompiled\CLIPPIT.acd";
-        var path = "/Users/wieslawsoltes/Documents/GitHub/Acdparser/clippitMS/CLIPPIT ACS Decompiled/CLIPPIT.acd";
         using var stream = File.OpenRead(path);
         var acd = AcdParser.ParseAcd(stream);
         if (acd is { })
@@ -33,7 +88,17 @@ public partial class MainWindow : Window
 
     private void ParseButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        ParseAsdFile();
+        
+        //var path = @"c:\Users\Administrator\Downloads\ACS\clippitMS\CLIPPIT ACS Decompiled\CLIPPIT.acd";
+        var path = "/Users/wieslawsoltes/Documents/GitHub/Acdparser/clippitMS/CLIPPIT ACS Decompiled/CLIPPIT.acd";
+
+        var basePath = Path.GetDirectoryName(path);
+        if (basePath is { })
+        {
+            ImageConverter.BasePath = basePath;
+        }
+        
+        ParseAsdFile(path);
     }
 
     private void TreeView_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
